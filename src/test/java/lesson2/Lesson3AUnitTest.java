009e7ac743b052c539662366d35dbfee2fa0e34a
sommovir <luca.coraci@gmail.com>
2021-03-30 16:54:48 +0200

    leeson 3 half way

--- src/test/java/lesson2/Lesson3AUnitTest.java
+++ src/test/java/lesson2/Lesson3AUnitTest.java
@@ -5,12 +5,11 @@
  */
 package lesson2;
 
-import it.cnr.paci.j4p.lesson2.esercizi.Author;
-import it.cnr.paci.j4p.lesson2.esercizi.Book;
-import it.cnr.paci.j4p.lesson3.esercizi.RegistrationManager;
+import it.cnr.paci.j4p.lesson3.esercizi.stringhe.Account;
+import it.cnr.paci.j4p.lesson3.esercizi.stringhe.RegistrationManager;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.text.DecimalFormat;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Disabled;
@@ -23,7 +22,7 @@ import org.junit.jupiter.api.TestMethodOrder;
  *
  * @author sommovir
  */
-//@Disabled
+@Disabled
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class Lesson3AUnitTest {
 
@@ -50,7 +49,7 @@ public class Lesson3AUnitTest {
         if (vote > 30) {
             System.out.println("\t\t\tYOUR VOTE: 30 e Lode !");
         } else {
-            System.out.println("\t\t\tYOUR VOTE: " + round(vote,1) + "/30");
+            System.out.println("\t\t\tYOUR VOTE: " + round(vote, 1) + "/30");
         }
 
         System.out.println("===========================================================\n\n");
@@ -58,6 +57,7 @@ public class Lesson3AUnitTest {
 
     @org.junit.jupiter.api.BeforeEach
     public void setUp() throws Exception {
+        RegistrationManager.clear();
     }
 
     @org.junit.jupiter.api.AfterEach
@@ -142,7 +142,7 @@ public class Lesson3AUnitTest {
         result = "\t\t\t\t[SUCCESS][+0.2]";
         vote += 0.2f;
     }
-    
+
     @Test
     @DisplayName("[Test Stringhe](1/3) Easy: 3.A.2 nome e cognome separati da uno spazio")
     public void test5(TestInfo info) {
@@ -160,6 +160,7 @@ public class Lesson3AUnitTest {
         result = "\t\t\t[SUCCESS][+0.2]";
         vote += 0.2f;
     }
+
     @Test
     @DisplayName("[Test Stringhe](2/3) High: 3.A.2 nome e cognome contiene spazio ma in posizione invalida")
     public void test6(TestInfo info) {
@@ -173,7 +174,7 @@ public class Lesson3AUnitTest {
         result = "[SUCCESS][+1.0]";
         vote += 1f;
     }
-    
+
     @Test
     @DisplayName("[Test Stringhe](3/3) Medium: 3.A.2 nome e cognome devono iniziare con la maiuscola")
     public void test7(TestInfo info) {
@@ -187,35 +188,156 @@ public class Lesson3AUnitTest {
         assertFalse(p1, "il nome-cognome contiene una minuscola sul nome");
         assertFalse(p2, "il nome-cognome contiene una minuscola sul cognome");
         assertTrue(p4, "il nome-cognome è corretto ma non viene validato");
-        
 
         result = "\t[SUCCESS][+0.5]";
         vote += 0.5f;
     }
-    
-    
+
     @Test
     @DisplayName("[Test Stringhe](1/2) Easy: 3.A.3 parse name")
     public void test8(TestInfo info) {
         String luca = RegistrationManager.parseNameFrom("Luca Coraci");
         message = info.getDisplayName();
         result = "\t\t\t\t\t\t[FAIL]";
-        assertEquals(luca,"Luca", "il nome non viene correttamente estrapolato");
+        assertEquals(luca, "Luca", "il nome non viene correttamente estrapolato");
         result = "\t\t\t\t\t\t[SUCCESS][+0.3]";
         vote += 0.3f;
     }
-    
+
     @Test
     @DisplayName("[Test Stringhe](2/2) Easy: 3.A.3 parse surname")
     public void test9(TestInfo info) {
-        String coraci = RegistrationManager.parseNameFrom("Luca Coraci");
+        String coraci = RegistrationManager.parseSurnameFrom("Luca Coraci");
         message = info.getDisplayName();
         result = "\t\t\t\t\t\t[FAIL]";
-        assertEquals(coraci,"Coraci", "il cognome non viene correttamente estrapolato");
+        assertEquals(coraci, "Coraci", "il cognome non viene correttamente estrapolato");
         result = "\t\t\t\t\t\t[SUCCESS][+0.3]";
         vote += 0.3f;
     }
-    
-    
+
+    @Test
+    @DisplayName("[Test Stringhe](1/3) Easy: 3.A.5 validazione numero di telefono vuoto/nullo")
+    public void test91(TestInfo info) {
+        message = info.getDisplayName();
+        result = "\t\t[FAIL]";
+        boolean p1 = RegistrationManager.isPhoneNumberValid("");
+        boolean p2 = RegistrationManager.isPhoneNumberValid(null);
+        assertTrue(p1, "Il numero è una stringa vuota ma non viene accettato");
+        assertTrue(p2, "Il numero è una riferimento nullo ma non viene accettato");
+        result = "\t\t[SUCCESS][+0.3]";
+        vote += 0.3f;
+    }
+
+    @Test
+    @DisplayName("[Test Stringhe](2/3) Medium: 3.A.5 validazione numero di telefono, no prefix")
+    public void test92(TestInfo info) {
+        message = info.getDisplayName();
+        result = "\t\t[FAIL]";
+        boolean p1 = RegistrationManager.isPhoneNumberValid("3285110522");
+        boolean p2 = RegistrationManager.isPhoneNumberValid("328 5110522");
+        boolean p3 = RegistrationManager.isPhoneNumberValid("328-5110522");
+
+        boolean p4 = RegistrationManager.isPhoneNumberValid("123 111111");
+        boolean p5 = RegistrationManager.isPhoneNumberValid("152144A255");
+        boolean p6 = RegistrationManager.isPhoneNumberValid(" 328574541");
+        boolean p7 = RegistrationManager.isPhoneNumberValid(" 3253562");
+
+        assertTrue(p1, "questo numero non è stato accettato, fatti delle domande: [3285110522]");
+        assertTrue(p2, "questo numero non è stato accettato, fatti delle domande: [328 5110522]");
+        assertTrue(p3, "questo numero non è stato accettato, fatti delle domande: [328-5110522]");
+
+        assertFalse(p4, "questo numero è stato accettato, fatti delle domande: [123 111111]");
+        assertFalse(p5, "questo numero è stato accettato, fatti delle domande: [152144A255]");
+        assertFalse(p6, "questo numero è stato accettato, fatti delle domande: [ 328574541]");
+        assertFalse(p7, "questo numero è stato accettato, fatti delle domande: [ 3253562]");
+
+        result = "\t\t[SUCCESS][+0.8]";
+        vote += 0.3f;
+    }
+
+    @Test
+    @DisplayName("[Test Stringhe](3/3) High: 3.A.5 validazione numero di telefono, with prefix")
+    public void test93(TestInfo info) {
+        message = info.getDisplayName();
+        result = "\t\t[FAIL]";
+        boolean p1 = RegistrationManager.isPhoneNumberValid("+39 3285110522");
+        boolean p2 = RegistrationManager.isPhoneNumberValid("+39328 5110522");
+        boolean p3 = RegistrationManager.isPhoneNumberValid("+39328-5110522");
+
+        boolean p4 = RegistrationManager.isPhoneNumberValid("393285550588");
+        boolean p5 = RegistrationManager.isPhoneNumberValid("++393332222222");
+
+        assertTrue(p1, "questo numero non è stato accettato, fatti delle domande: [+39 328511052]");
+        assertTrue(p2, "questo numero non è stato accettato, fatti delle domande: [+39328 5110522]");
+        assertTrue(p3, "questo numero non è stato accettato, fatti delle domande: [+39328-5110522]");
+
+        assertFalse(p4, "questo numero è stato accettato, fatti delle domande: [393285550588]");
+        assertFalse(p5, "questo numero è stato accettato, fatti delle domande: [++393332222222]");
+
+        result = "\t\t[SUCCESS][+2.0]";
+        vote += 2.0f;
+    }
+
+    @Test
+    @DisplayName("[Test Stringhe](1/2) Medium: 3.A.6 username in uso. Test, Insert, test, test2")
+    public void test94(TestInfo info) {
+        message = info.getDisplayName();
+        result = "\t\t[FAIL]";
+        Account account = new Account("Carlo", "Carloni", "carlotto", "carl8superstar");
+        boolean p1 = RegistrationManager.isUsernameAlreadyInUse("carlotto");
+        RegistrationManager.addAccount(account);
+        boolean p2 = RegistrationManager.isUsernameAlreadyInUse("carlotto");
+        boolean p3 = RegistrationManager.isUsernameAlreadyInUse("carlotto2");
+
+        assertFalse(p1, "non ho inserito ancora nessuno e già mi dice che un username è in uso!");
+        assertTrue(p2, "dopo aver inserito un account, riprovo con lo stesso username e dovrebbe dirmi che è già in uso");
+        assertFalse(p3, "dopo aver inserito un account, riprovo con un username diverso e mi dovrebbe dire che non è in uso");
+
+        result = "\t\t[SUCCESS][+1.0]";
+        vote += 1.0f;
+    }
+
+    @Test
+    @DisplayName("[Test Stringhe](2/2) Medium: 3.A.6 username in uso. test, insert, test, clear, test")
+    public void test95(TestInfo info) {
+        message = info.getDisplayName();
+        result = "\t[FAIL]";
+        Account account = new Account("Carlo", "Carloni", "carlotto", "carl8superstar");
+        boolean p1 = RegistrationManager.isUsernameAlreadyInUse("carlotto"); //false
+        RegistrationManager.addAccount(account);
+        boolean p2 = RegistrationManager.isUsernameAlreadyInUse("carlotto"); //true
+        RegistrationManager.clear();
+        boolean p3 = RegistrationManager.isUsernameAlreadyInUse("carlotto"); //false
+
+        assertFalse(p1, "non ho inserito ancora nessuno e già mi dice che un username è in uso!");
+        assertTrue(p2, "dopo aver inserito un account, riprovo con lo stesso username e dovrebbe dirmi che è già in uso");
+        assertFalse(p3, "dopo aver eseguito un clear, riprovo con lo stesso username precedente e dovrebbe dirmi che è disponibile, probabile errore nella clear");
+
+        result = "\t[SUCCESS][+1.0]";
+        vote += 1.0f;
+    }
+
+    @Test
+    @DisplayName("[Test Stringhe](1/1) Medium: 3.A.7 clear Test: insert, insert, test, clear, test")
+    public void test96(TestInfo info) {
+        message = info.getDisplayName();
+        result = "\t[FAIL]";
+        Account account = new Account("Carlo", "Carloni", "carlotto", "carl8superstar");
+        Account account2 = new Account("Carlo2", "Carloni2", "carlotto2", "carl8superstar2");
+        RegistrationManager.addAccount(account);
+        RegistrationManager.addAccount(account2);
+        List<Account> accounts = RegistrationManager.getAccounts();
+        assertNotNull(accounts, "Ho inserito due utenti ma mi ritorna una lista nulla, controllare la 'getAccounts()'");
+        int size = accounts.size();
+        assertEquals(2, size, "Ho inserito due accounts ma non mi da 2 come dimensione della lista");
+        RegistrationManager.clear();
+        List<Account> accounts2 = RegistrationManager.getAccounts();
+        assertNotNull(accounts2, "Ho inserito due utenti ed eseguito un clear ma mi ritorna una lista nulla, controllare la 'getAccounts()'");
+        int size2 = accounts2.size();
+        assertEquals(0, size2, "Ho inserito due accounts e poi eseguito un clear, la dimensione della lista utenti dovrebbe essere 0");
+        
+        result = "\t[SUCCESS][+1.0]";
+        vote += 1.0f;
+    }
 
 }
