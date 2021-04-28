799ac06df5285aa491a3f3da6a6e048c52117cfd
sommovir <luca.coraci@gmail.com>
2021-04-28 04:22:46 +0200

    example

---
+++ src/main/java/it/cnr/paci/j4p/lesson5/eccezioni/example0/Example01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.cnr.paci.j4p.lesson5.eccezioni.example0;
+
+/**
+ *
+ * @author sommovir
+ */
+public class Example01 {
+    
+    
+    public static int divide(int a, int b){
+        return a/b;
+    }
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Buongiorno");
+        int c = divide(6, 0);
+        System.out.println("c = "+c);
+        System.out.println("Ora vi saluto");
+        
+        
+    }
+    
+}
